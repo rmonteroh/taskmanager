c0d42037ed1840214b1c8f7ead6182d328db24b0
rmonteroh <rmonteroh90@gmail.com>
2020-05-14 02:08:32 -0500

    Seting up config files

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
 
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  logger.log(`Aplecation run on port 300`);
+  // Habilitando cors
+  if (process.env.NODE_ENV === 'development') {
+    logger.log('App runing on development mode :)')
+    app.enableCors();
+  }
+
+  const serverConfig = config.get('server');  
+  const port = process.env.PORT || serverConfig.port;
+  await app.listen(port);
+  
+  logger.log(`Aplication run on port ${port}`);
 }
 bootstrap();
